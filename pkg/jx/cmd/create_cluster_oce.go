bc98909cd805277a9085375600d8f63d77b4535d
hui_liu <hui.liu@oracle.com>
2018-06-25 16:41:53 +0800

    update log library

--- pkg/jx/cmd/create_cluster_oce.go
+++ pkg/jx/cmd/create_cluster_oce.go
@@ -12,10 +12,10 @@ import (
 	"time"
 
 	"github.com/Pallinder/go-randomdata"
-	"github.com/jenkins-x/jx/pkg/jx/cmd/log"
 	"github.com/jenkins-x/jx/pkg/jx/cmd/oce"
 	"github.com/jenkins-x/jx/pkg/jx/cmd/templates"
 	cmdutil "github.com/jenkins-x/jx/pkg/jx/cmd/util"
+	"github.com/jenkins-x/jx/pkg/log"
 	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/spf13/cobra"
 	"gopkg.in/AlecAivazis/survey.v1"
